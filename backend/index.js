2662e8f9f8fa91bd08b9d67a7175bdf53680ad40
Tarun <thakurtarun1012@gamil.com>
2026-03-13 15:53:04 +0530

    b3

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,15 @@
 const path = require('path');
- require('dotenv').config();
+require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const fs = require('fs');
 
 const app = express();
 
 app.use(cors({
   origin: ["http://localhost:5173","https://ecommerceshophubb.onrender.com"],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"],
   credentials: true
 }));
 
@@ -21,22 +20,21 @@ const categoryRoutes = require('./routes/categoryRoutes');
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("mongodb is connect"))
-  .catch(err => console.log("error :", err));
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>console.log("mongodb is connect"))
+.catch(err=>console.log("error :",err));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/product', productRoutes);
 app.use('/api/order', orderRoutes);
 
-app.use(express.static(path.join(__dirname, "dist")));
+/* frontend serve */
+app.use(express.static(path.join(__dirname,"../frontent/ecommerce-frontent/dist")));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontent/ecommerce-frontent/dist/index.html"));
+app.get(/.*/, (req,res)=>{
+  res.sendFile(path.join(__dirname,"../frontent/ecommerce-frontent/dist/index.html"));
 });
 
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
+app.listen(PORT, ()=>console.log(`Server start on port ${PORT}`));
