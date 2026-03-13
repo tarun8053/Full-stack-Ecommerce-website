c9ec5cd10022d456a94d27639e3efef42f6f59d4
Tarun <thakurtarun1012@gamil.com>
2026-03-13 15:45:01 +0530

    b2

--- backend/index.js
+++ backend/index.js
@@ -33,8 +33,8 @@ app.use('/api/order', orderRoutes);
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontent/ecommerce-frontent/dist/index.html"));
 });
 
 
