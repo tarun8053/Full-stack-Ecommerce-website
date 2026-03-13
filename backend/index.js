2f8c4d1eea742b35ea05cccdf5236de4f83d9d4c
Tarun <thakurtarun1012@gamil.com>
2026-03-13 15:02:21 +0530

    CORS

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173","https://ecommerceshophubb.onrender.com"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
